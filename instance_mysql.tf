9f66d1ca213711bf07c11dce8db202702ef5c912
beamsoul <nurzhanasamidinova@gmail.com>
2019-08-03 11:08:48 -0700

    deleted a vpc  security group from MYSQL

--- instance_mysql.tf
+++ instance_mysql.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "mysql" {
     key_name = "${var.key_name}"
     subnet_id = "${aws_subnet.public.id}"
     user_data = "${file("userdata.sh")}"
-    vpc_security_group_ids = ["${aws_security_group.privateDB.id}"]
     associate_public_ip_address = "true"
     tags = {
       Name = "${var.Name}.MYSQL"
