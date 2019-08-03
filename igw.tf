917dc744fea42071dcf5966d2781e5a905849cf6
beamsoul <nurzhanasamidinova@gmail.com>
2019-08-03 09:47:20 -0700

    push gateway

---
+++ igw.tf
@@ -0,0 +1,11 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.dev.id}"
+
+  
+  tags = {
+      Name = "${var.Name}"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}
