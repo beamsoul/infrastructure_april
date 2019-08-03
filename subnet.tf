741def382273b23922181acd7104b1858f3072f4
beamsoul <nurzhanasamidinova@gmail.com>
2019-08-03 10:05:12 -0700

    adding

--- subnet.tf
+++ subnet.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "public" {
 
 
 resource "aws_subnet" "private" {
-  vpc_id     = "${aws_vpc.dev.id}"
+  vpc_id     = "${aws_vpc.r.id}"
   cidr_block = "10.0.2.0/24"
 
   tags = {
