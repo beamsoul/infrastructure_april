91f6abb7b81c1bb3dea9805adf5e8c6fcffa80f4
beamsoul <nurzhanasamidinova@gmail.com>
2019-08-04 12:44:20 -0700

    pushing the modules

--- bucket.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bucketname" {
-    bucket = "infrastructure-april-janna-2019a"
-  
-}
--- dont_create.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bucketnameb" {
-    bucket = "infrastructure-april-janna-2019b"
-  
-}
