5760c07557a517552b1cff06afdbac456e010ca8
beamsoul <nurzhanasamidinova@gmail.com>
2019-08-04 12:16:02 -0700

    pushing the

---
+++ bucket.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "bucketname" {
+    bucket = "infrastructure-april-janna-2019a"
+  
+}
---
+++ dont_create.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "bucketnameb" {
+    bucket = "infrastructure-april-janna-2019b"
+  
+}
